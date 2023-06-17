72c6b5d2a98fe33e35633e3703a312d45997a728
DavidTMaciel <davidmacieljr122@gmail.com>
2023-06-17 13:15:01 -0300

    Create router user/id

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,7 @@ const router = Router();
 
 router.post("/user", UserController.createUser);
 router.get("/users", UserController.findAllUsers);
+router.get("/user/:id", UserController.findUser);
 
 
 
