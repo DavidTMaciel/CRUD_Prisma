a372783c1d5299d174f179dbebfca4b336463f1b
DavidTMaciel <davidmacieljr122@gmail.com>
2023-06-19 19:25:21 -0300

    Add route Updater user

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,8 @@ const router = Router();
 router.post("/user", UserController.createUser);
 router.get("/users", UserController.findAllUsers);
 router.get("/user/:id", UserController.findUser);
+router.post("/user/update/:id", UserController.updateUser);
+
 
 
 
